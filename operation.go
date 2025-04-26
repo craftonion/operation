10f4505f6651cd4a9b33af71a4951dfb994d0896
mkz <makazhur@yandex.ru>
2025-04-26 11:53:43 +0500

    add comment

--- operation.go
+++ operation.go
@@ -1,5 +1,7 @@
 package operation
 
+// Add Bringing two or more numbers (or things) together to make a new total
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
